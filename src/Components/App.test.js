fb6cdad7e2621d03e6dcc63fc54a31eba4fc2541
Lee Hyungi <hyungi.lee.622@gmail.com>
2021-05-02 01:15:05 +0900

    test: Modify test code for checking header menu links

--- src/Components/App.test.js
+++ src/Components/App.test.js
@@ -10,15 +10,15 @@ describe('<App /> component', () => {
         render(<App/>);
     });
     test('렌더링된 <App />컴포넌트의 header에 Movie 메뉴가 존재한다.', () => {
-        const movieLinkEl = screen.getByText(/Movie/);
+        const movieLinkEl = screen.getByText("Movie");
         expect(movieLinkEl).toBeInTheDocument;
     });
     test('렌더링된 <App />컴포넌트의 header에 TV 메뉴가 존재한다.', () => {
-        const movieLinkEl = screen.getByText(/TV/);
-        expect(movieLinkEl).toBeInTheDocument;
+        const tvLinkEl = screen.getByText("TV");
+        expect(tvLinkEl).toBeInTheDocument;
     });
     test('렌더링된 <App />컴포넌트의 header에 Search 메뉴가 존재한다.', () => {
-        const movieLinkEl = screen.getByText(/Search/);
-        expect(movieLinkEl).toBeInTheDocument;
+        const searchLinkEl = screen.getByText("Search");
+        expect(searchLinkEl).toBeInTheDocument;
     });
 });
