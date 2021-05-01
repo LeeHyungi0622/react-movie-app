514b4067efbc3efe07714c881b69279437411622
Lee Hyungi <hyungi.lee.622@gmail.com>
2021-05-02 00:44:24 +0900

    test: Write test code to check the existence of main menu link
    - App component의 rendering 유무확인 테스트 코드작성
    - header에 Movie, TV, Search 메뉴링크 유무확인 테스트 코드작성

--- src/Components/App.test.js
+++ src/Components/App.test.js
@@ -9,4 +9,16 @@ describe('<App /> component', () => {
     test('<App /> 컴포넌트가 문제없이 rendering된다.',() => {
         render(<App/>);
     });
+    test('렌더링된 <App />컴포넌트의 header에 Movie 메뉴가 존재한다.', () => {
+        const movieLinkEl = screen.getByText(/Movie/);
+        expect(movieLinkEl).toBeInTheDocument;
+    });
+    test('렌더링된 <App />컴포넌트의 header에 TV 메뉴가 존재한다.', () => {
+        const movieLinkEl = screen.getByText(/TV/);
+        expect(movieLinkEl).toBeInTheDocument;
+    });
+    test('렌더링된 <App />컴포넌트의 header에 Search 메뉴가 존재한다.', () => {
+        const movieLinkEl = screen.getByText(/Search/);
+        expect(movieLinkEl).toBeInTheDocument;
+    });
 });
