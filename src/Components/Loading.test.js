aa348a6697c5f8657cf2d622f4e06eaf0327d56a
Lee Hyungi <hyungi.lee.622@gmail.com>
2021-05-02 15:57:47 +0900

    test: Modify test code (Loading component)

--- src/Components/Loading.test.js
+++ src/Components/Loading.test.js
@@ -6,11 +6,8 @@ describe('<Loading /> 컴포넌트', () => {
     test('<Loading /> 컴포넌트가 문제없이 렌더링되는지 확인', () => {
         render(<Loading />);
     });
-    test('렌더링된 <Loading /> 컴포넌트의 내부에 spinner와 텍스트가 있는지 확인', () => {
-        render(<Loading />);
-        const { getByTestId } = render(
-            <Loading />
-        );
+    test('렌더링된 <Loading /> 컴포넌트의 내부에 spinner가 존재하는지 확인', () => {
+        const { getByTestId } = render(<Loading />);
         expect(getByTestId('spinner')).toBeTruthy();
         expect(getByTestId('loading-text')).toBeTruthy();
     });
