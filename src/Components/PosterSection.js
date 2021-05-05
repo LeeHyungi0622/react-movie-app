050989cf4164f4957c1c47ca7337d9b39bf31316
Lee Hyungi <hyungi.lee.622@gmail.com>
2021-05-05 15:20:07 +0900

    feat: Partially completed movie component
    - 현재 상영중인 영화 컨텐츠 정보출력 완료
    - 기존의 Poster 및 PosterSection 컴포넌트 수정

--- src/Components/PosterSection.js
+++ src/Components/PosterSection.js
@@ -6,13 +6,16 @@ const SectionContainer = styled.section`
 `;
 
 const Title = styled.h2`
-
+    font-size: 17px;
+    font-weight: bold;
 `;
 
 const GridChildren = styled.div`
     display: grid;
     grid-template-columns: repeat(auto-fill, 125px);
-    grid-gap: 25px;
+    grid-gap: 30px;
+    margin-top: 14px;
+    
 `;
 
 const PosterSection = ({ title, children }) => {
